06810893f7115f2e3055fb436f65bc86d49014be
abdulrehman640 <mh4167891@gmail.com>
2026-05-20 23:01:00 +0500

    tags

---
+++ C++Language/secondfolder/table of number.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    int y, i = 1 ;
+    
+    y =7;
+    while (i != 11)
+    {
+        cout<< y << " x " << i <<" = " << y * i << endl;
+        i += 1;
+    }
+
+    for(int i = 0; i <= 10; i++){
+        cout << "              hello " ;
+    }
+    do {
+     
+        cout << "hi";
+
+    }while ( i != 1);
+
+}
